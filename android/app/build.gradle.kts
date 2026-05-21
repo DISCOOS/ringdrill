cf20a287ed73a89aec96d9cee9ef1b8b867eccbe
Kenneth Gulbrandsøy <kenneth@discoos.org>
2026-05-21 18:29:45 +0200

    Upgraded kotlin dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,17 +19,13 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "org.discoos.ringdrill"
-    compileSdk = 35
-    ndkVersion = "27.0.12077973" //flutter.ndkVersion
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
@@ -64,7 +60,7 @@ android {
         //implementation("androidx.window:window:1.4.0")
         //implementation("androidx.window:window-java:1.4.0")
         coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
-        implementation("com.google.android.material:material:1.12.0")
+        implementation("com.google.android.material:material:1.14.0")
     }
 }
 
